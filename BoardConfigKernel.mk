20de0c67dbe19732a503e011ece2708c011033cb
Reinazhard <reinazhard@gmail.com>
2023-06-08 16:35:36 +0200

    bouquet: Update kernel
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- BoardConfigKernel.mk
+++ BoardConfigKernel.mk
@@ -1,9 +1,15 @@
+KERNEL_DIR := device/xiaomi/bouquet-kernel
+
+KERNEL_MODULES := $(wildcard $(KERNEL_DIR)/*.ko)
+BOARD_VENDOR_KERNEL_MODULES := $(KERNEL_MODULES)
+
+
 # Let it fail if it doesn't match the device
 ifeq ($(TARGET_DEVICE),whyred)
-    LOCAL_KERNEL := device/xiaomi/bouquet-kernel/whyred-Image.gz-dtb
+    LOCAL_KERNEL := $(KERNEL_DIR)/whyred-Image.gz-dtb
 endif
 ifeq ($(TARGET_DEVICE),tulip)
-    LOCAL_KERNEL := device/xiaomi/bouquet-kernel/tulip-Image.gz-dtb
+    LOCAL_KERNEL := $(KERNEL_DIR)/tulip-Image.gz-dtb
 endif
 
 PRODUCT_COPY_FILES += \
