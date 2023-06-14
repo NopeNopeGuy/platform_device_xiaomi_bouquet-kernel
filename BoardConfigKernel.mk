9096cb663a817ddb9361dd9e886cbcafe60c50c9
NopeNope <kingdutchisbad+12@proton.me>
2023-06-14 18:09:10 +0530

    kernel: Don't give af about device

--- BoardConfigKernel.mk
+++ BoardConfigKernel.mk
@@ -4,13 +4,8 @@ KERNEL_MODULES := $(wildcard $(KERNEL_DIR)/*.ko)
 BOARD_VENDOR_KERNEL_MODULES := $(KERNEL_MODULES)
 
 
-# Let it fail if it doesn't match the device
-ifeq ($(TARGET_DEVICE),whyred)
-    LOCAL_KERNEL := $(KERNEL_DIR)/whyred-Image.gz-dtb
-endif
-ifeq ($(TARGET_DEVICE),tulip)
-    LOCAL_KERNEL := $(KERNEL_DIR)/tulip-Image.gz-dtb
-endif
+LOCAL_KERNEL := $(KERNEL_DIR)/whyred-Image.gz-dtb
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
