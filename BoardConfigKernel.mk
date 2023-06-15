94424b76edaebef452b78aa21ef3e2e4cd3f7cb3
NopeNope <kingdutchisbad+12@proton.me>
2023-06-15 15:08:28 +0530

    kernel: Maybe work?

--- BoardConfigKernel.mk
+++ BoardConfigKernel.mk
@@ -3,9 +3,5 @@ KERNEL_DIR := device/xiaomi/bouquet-kernel
 KERNEL_MODULES := $(wildcard $(KERNEL_DIR)/*.ko)
 BOARD_VENDOR_KERNEL_MODULES := $(KERNEL_MODULES)
 
+TARGET_PREBUILT_KERNEL := $(KERNEL_DIR)/Image.gz-dtb
 
-LOCAL_KERNEL := $(KERNEL_DIR)/Image.gz-dtb
-
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
